2a94c51bea667214fb78145c300868e96d22db6c
Lasni Rathnayaka <lasni.dev@gmail.com>
2026-06-09 13:37:50 +0530

    first commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,48 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+import "../../global.css";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-background">
+      <Text className="text-xl font-bold text-success">
+        Welcome to Nativewind!
+      </Text>
+      <Link
+        href="/onBoarding"
+        className="mt-4 rounded bg-primary text-white p-4"
+      >
+        Go to OnBording
+      </Link>
+
+      <Link
+        href="/(auth)/sign-in"
+        className="mt-4 rounded bg-primary text-white p-4"
+      >
+        Go to Sign In
+      </Link>
+      <Link
+        href="/(auth)/sign-up"
+        className="mt-4 rounded bg-primary text-white p-4"
+      >
+        Go to Sign Up
+      </Link>
+
+      <Link
+        href="/subscriptions/spotify"
+        className="mt-4 rounded bg-primary text-white p-4"
+      >
+        Spotify Subscription
+      </Link>
+      <Link
+        href={{
+          pathname: "/(tabs)/subscriptions/[id]",
+          params: { id: "claude" },
+        }}
+        className="mt-4 rounded bg-primary text-white p-4"
+      >
+        Claude Max Subscription
+      </Link>
+    </View>
+  );
+}
