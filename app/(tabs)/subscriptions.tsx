7327efbe995a81966cbd6ac7546ea4570a9da9fc
Lasni Rathnayaka <lasni.dev@gmail.com>
2026-06-12 13:50:04 +0530

    Test Rabit code

--- app/(tabs)/subscriptions.tsx
+++ app/(tabs)/subscriptions.tsx
@@ -1,6 +1,7 @@
+import images from "@/constants/images";
 import { styled } from "nativewind";
 import React from "react";
-import { Text } from "react-native";
+import { Image, Text } from "react-native";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
 const SafeAreaView = styled(RNSafeAreaView);
 
@@ -8,6 +9,7 @@ const Subscriptions = () => {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
       <Text>Subscriptions </Text>
+      <Image source={images.avatar} className="mx-auto my-auto" />
     </SafeAreaView>
   );
 };
